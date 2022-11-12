d1c79f9a4e942661da3c0ec616140385fd4d2f47
sajeed-syn <sajeedrs@gmail.com>
2022-11-12 17:26:41 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { SimpleComponent } from './simple/simple.component';
+import { MovieaddComponent } from './movieadd/movieadd.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoComponent,
+    SimpleComponent,
+    MovieaddComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/simple/simple.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { MessageService } from '../message.service';
+@Component({
+  selector: 'app-simple',
+  templateUrl: './simple.component.html',
+  styleUrls: ['./simple.component.css']
+})
+export class SimpleComponent implements OnInit {
+  weekdays: string[] = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  age: number = 20
+
+  emplist: Employee[] = [
+    { id: 1010, name: 'Tom', salary: 60000, dept: 'IT' },
+    { id: 1020, name: 'Bob', salary: 70000, dept: 'HR' },
+    { id: 1030, name: 'Ali', salary: 50000, dept: 'Fin' },
+    { id: 1040, name: 'Sam', salary: 40000, dept: 'IT' },
+    { id: 1050, name: 'Raj', salary: 30000, dept: 'Fin' },
+    { id: 1060, name: 'Jay', salary: 80000, dept: 'HR' }
+  ]
+  GetTotal(): number {
+    var total: number = 0
+    for (var i = 0; i < this.emplist.length; i++) {
+      total = total + this.emplist[i].salary
+    }
+    return total
+  }
+  constructor(private msgservice: MessageService) { }
+
+  msg = this.msgservice.Display('Tom')
+
+  ngOnInit(): void {
+  }
+
+}
